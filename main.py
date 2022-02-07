9260d31165106487a92072d64b7175c28d27a8a7
Userse0330 <99172897+Userse0330@users.noreply.github.com>
2022-02-07 13:39:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import time
 import sys
 
+
 name = "Test."
 print(chr(27) + "[2J")
 username = input("What is your name: ")
@@ -11,62 +12,18 @@ order = input()
 price = 60
 quantity = input("How many would you like:\n")
 total = price * int(quantity)
-print("your total is:", total)
-time.sleep(3)
+print("You ordered:", quantity, order)
+print("your total is:",total)
+time.sleep(8)
 print(chr(27) + "[2J")
 print("Preparing your", order)
 time.sleep(10)
 print(chr(27) + "[2J")
 thankyou = "Thank you for shopping at Simple_Barista."
-time.sleep(3)
+time.sleep(8)
 for char in thankyou:
     time.sleep(0.1)
     sys.stdout.write(char)
     sys.stdout.flush()
 print(chr(27) + "[2J")
 #Made by sebastian
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#MIT License
-
-#Copyright (c) 2022 SE0330
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom #the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.

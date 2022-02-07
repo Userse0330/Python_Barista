8b4bc9d0176d4dca0257d8f5925683bffc775ddb
Userse0330 <99172897+Userse0330@users.noreply.github.com>
2022-02-07 12:57:07 +0100

    Updated to 1.1
    Improved the usability and functionality of the script.

--- main.py
+++ main.py
@@ -1,54 +1,45 @@
+start = ''
 #Import Modules
 import time
 import sys
 
 #Variables
 name = "Test."
-username = input("What is your name? ")
-menu = "Espresso", "Cappuccino", "Mocha",""
-print(username,"What would you like to order? Take a look at our menu.", menu)
+
+#Start Script
+print(chr(27) + "[2J")
+username = input("What is your name: ")
+menu = "Espresso", "Cappuccino", "Mocha","Latte","Frappe"
+print("What would you like to order",username,"?","Take a look at our menu:", menu)
 order = input()
 #Price Variable
 price = 60
 
 #Main Script
-quantity = input("How many would you like?\n")
+quantity = input("How many would you like:\n")
 total = price * int(quantity)
-print("your total is", total)
+print("your total is:", total)
 time.sleep(3)
+print(chr(27) + "[2J")
 print("Preparing your", order)
 time.sleep(10)
-print("Here is your", quantity,order)
+print(chr(27) + "[2J")
+thankyou = "Thank you for shopping at Simple_Barista."
+endmessage = "Made by se0330."
+
+#End Script
 time.sleep(3)
-
-thankyou = "Thank you for shopping at Testshop"
-ewasdf = "#####################"
-sadfasdf = "Made by sebastian"
-
-
-time.sleep(3)
-
 for char in thankyou:
     time.sleep(0.1)
     sys.stdout.write(char)
     sys.stdout.flush()
 
 print(chr(27) + "[2J")
-
-for char in ewasdf:
-    time.sleep(0.2)
+time.sleep(2)
+time.sleep(3)
+for char in endmessage:
+    time.sleep(0.1)
     sys.stdout.write(char)
     sys.stdout.flush()
-
-print(chr(27) + "[2J")
-
-for char in sadfasdf:
-    time.sleep(0.2)
-    sys.stdout.write(char)
-    sys.stdout.flush()
-
-print(chr(27) + "[2J")
-
-
 
 #Made by Sebastian

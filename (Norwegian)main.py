23bc29101f296915f0b7f17494952a145e35f152
Userse0330 <99172897+Userse0330@users.noreply.github.com>
2022-02-09 08:52:07 +0100

    Updated (Norwegian) Main.py
    Updated a couple of things and added some new functionality and other stuff.

--- (Norwegian)main.py
+++ (Norwegian)main.py
@@ -11,60 +11,50 @@ menu = "Espresso", "Cappuccino", "Mocha","Latte","Frappe"
 
 name = "Test."
 print(chr(27) + "[2J")
-
-print("Anmeldelsene plassen har fått:")
-
-print("Nydelig, koselig og fin plass å dra til etter en hard og lang arbeidsdag 5/5.")
-
-print("Kjempe trang, gammel og skitten plass. Bestilte en kaffe som de sølte overalt og jeg måtte betale 20 kroner ekstra, anbefales ikke. 2/5")
-
-print("Man kan ikke bestille mere en 10 kaffekopper før de søler overalt og du må betale 20 kroner ekstra! Ikke dra hit. 1/5")
-
-print("Veldig koselig og trivelig sted du kan dra til når som helst :) 5/5")
-
-print("God Cappuccino 4/5")
-
- 
-
+welcomemessage = "Velkommen til Simple_Barista"
+print(welcomemessage)
 username = input("Hva er navnet ditt? ")
-print("Hva vil du ha",username,"?","Ta en titt på menyen:", menu)
+print("Hva vil du bestille",username,"?","Her er menyen våres:", menu)
 order = input()
 price = 60
+sure = input("Er du sikker på at du vil fortsette? ")
+if sure=="nei" or "Nei" or "stop" or "Stop":
+ sys.exit("User Stopped.")
 
 print(chr(27) + "[2J")
-quantity = input("Hvor mange vil du ha?\n")
+quantity = input("Hvor mange\n",order,"vil du ha?")
 total = price * int(quantity)
 print("Du bestilte:", quantity, order)
-print("total sum:",total)
+print("totalen blir:",total)
 time.sleep(4)
 print(chr(27) + "[2J")
 print("Forbedrer din", order)
 time.sleep(5)
 print(chr(27) + "[2J")
-print("done")
+print("Ferdig")
 time.sleep(2)
 print(chr(27) + "[2J")
-choices = "Ansett advokater mot bedriften","Forlat butikken og miste",price
+choices = "Ansette advokater mot selskapet","Forlat stedet men du mister",int(total) * 2
 randomnumber = 5
 
 if int(quantity) > 50:
-  print("Å nei! Jeg sølte din kaffe. Her er dine valg:" ,choices)
-  asdasdasdgsdf = input("Hva vil du gjøre? ")
+  print("Å nei! Jeg sølte din kaffe. Her er valgene dine:" ,choices)
+  lawyerinput = input("Hva velger du å gjøre? ")
 else:
-   print("Hade! Kom tilbake når du vil.")
+   print("Hade! Kom tilbake senere.")
    sys.exit("User done")
 
 
 
-if asdasdasdgsdf=="Ansett advokater" or "Ansett Advokater" or "advokat" or "Advokat":
+if lawyerinput=="Ansett advokater" or "ansett advokater" or "advokat" or "Advokat" or "Advokater" or "advokater" or "ansett advokat" or "Ansett advokat" or "Ansett Advokat":
   value1 = random.randint(0,10)
-  if value1== 4:
-    print("Du tapte og må betale",price *2)
+  if value1 < 4:
+    print("Du tapte og mistet",total *2)
   if int(value1) > 4:
-    print("Du vant og fikk tilbake",price)
-elif asdasdasdgsdf=="Forlat" or "forlat":
+    print("Du forlot stedet og fikk",total,"tilbake.")
+elif lawyerinput=="Leave" or "leave":
   print("Du forlot stedet og mistet",price)
 
-print("Hade, kom tilbake når du vil!")
+
 time.sleep(5)
 print(chr(27) + "[2J")

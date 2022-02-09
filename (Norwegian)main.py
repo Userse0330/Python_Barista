8bd153db75848ced139b805b6e2cae10a0607fd2
Userse0330 <99172897+Userse0330@users.noreply.github.com>
2022-02-09 09:05:43 +0100

    Updated norwegian_main.py
    Fixed some bugs and added a few more things, updated to the same version as the English version.

--- (Norwegian)main.py
+++ (Norwegian)main.py
@@ -11,21 +11,21 @@ menu = "Espresso", "Cappuccino", "Mocha","Latte","Frappe"
 
 name = "Test."
 print(chr(27) + "[2J")
-welcomemessage = "Velkommen til Simple_Barista"
+welcomemessage = "Welcome to Simple_Barista"
 print(welcomemessage)
 username = input("Hva er navnet ditt? ")
-print("Hva vil du bestille",username,"?","Her er menyen våres:", menu)
+print("Hva vil du bestille",username,"?","her er menyen:", menu)
 order = input()
 price = 60
 sure = input("Er du sikker på at du vil fortsette? ")
-if sure=="nei" or "Nei" or "stop" or "Stop":
+if sure=="Nei":
  sys.exit("User Stopped.")
 
 print(chr(27) + "[2J")
-quantity = input("Hvor mange\n",order,"vil du ha?")
+quantity = input("Hvor mange vil du ha?\n")
 total = price * int(quantity)
 print("Du bestilte:", quantity, order)
-print("totalen blir:",total)
+print("Totalen blir:",total)
 time.sleep(4)
 print(chr(27) + "[2J")
 print("Forbedrer din", order)
@@ -34,11 +34,11 @@ print(chr(27) + "[2J")
 print("Ferdig")
 time.sleep(2)
 print(chr(27) + "[2J")
-choices = "Ansette advokater mot selskapet","Forlat stedet men du mister",int(total) * 2
+choices = "Ansett Advokater mot stedet","Forlat stedet men du msiter",int(total) * 2
 randomnumber = 5
 
 if int(quantity) > 50:
-  print("Å nei! Jeg sølte din kaffe. Her er valgene dine:" ,choices)
+  print("Å nei! Jeg sølte din kaffe. Her er dine valg:" ,choices)
   lawyerinput = input("Hva velger du å gjøre? ")
 else:
    print("Hade! Kom tilbake senere.")
@@ -46,14 +46,14 @@ else:
 
 
 
-if lawyerinput=="Ansett advokater" or "ansett advokater" or "advokat" or "Advokat" or "Advokater" or "advokater" or "ansett advokat" or "Ansett advokat" or "Ansett Advokat":
+if lawyerinput=="Advokat" or "advokat" or "ansett advokat" or "Ansett Advokat" or "Ansett Advokat" or "Advokater" or "advokater" or "ansett advokater":
   value1 = random.randint(0,10)
   if value1 < 4:
     print("Du tapte og mistet",total *2)
   if int(value1) > 4:
-    print("Du forlot stedet og fikk",total,"tilbake.")
-elif lawyerinput=="Leave" or "leave":
-  print("Du forlot stedet og mistet",price)
+    print("Du forlot steted og du fikk",total,"tilbake.")
+elif lawyerinput=="forlat" or "Forlat":
+  print("Du forlot stedet og mistet",total * 2)
 
 
 time.sleep(5)
